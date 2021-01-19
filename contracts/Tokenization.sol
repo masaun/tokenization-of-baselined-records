644e76d9bbf69d5374f4767c0f01c3e1e101a4cc
masaun <masanoriuno@gmail.com>
2021-01-19 11:53:38 +0900

    Define BrToken.sol and Tokenization.sol

---
+++ contracts/Tokenization.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6.9;
+pragma experimental ABIEncoderV2;
+
+/***
+ * @title - Tokenization contract
+ * @notice - This is the factory contract in order to create BrToken
+ **/
+contract Tokenization {
+
+    constructor() public {}
+
+}
