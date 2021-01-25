d1135e11d46ed29b44e4884556182205e6260456
masaun <masanoriuno@gmail.com>
2021-01-25 10:02:07 +0900

    Update depositETH() (<=Add the transferring-ETH method)

--- contracts/OrgPool.sol
+++ contracts/OrgPool.sol
@@ -12,7 +12,10 @@ contract OrgPool {
     /**
      * @notice - An organization deposit ETH into the Organization Pool
      */
-    function depositETH() public returns (bool) {}
+    function depositETH() public returns (bool) {
+        address orgAddress = msg.sender;
+        orgAddress.transfer(msg.value);
+    }
 
     /**
      * @notice - Share gas cost between registered-organizations
