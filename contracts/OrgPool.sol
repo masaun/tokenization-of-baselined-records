c5132391f043823572e19430a28869a3c09eaa31
masaun <masanoriuno@gmail.com>
2021-01-25 10:41:53 +0900

    [Success]: OrgPool contract call ETH from an user's wallet (by using contractAddr.call.value())

--- contracts/OrgPool.sol
+++ contracts/OrgPool.sol
@@ -7,14 +7,25 @@ pragma experimental ABIEncoderV2;
  **/
 contract OrgPool {
 
-    constructor() public {}
+    address ORG_POOL;
+    address payable ORG_POOL_PAYABLE;
+
+    constructor() public {
+        ORG_POOL = address(this);
+        ORG_POOL_PAYABLE = address(uint160(ORG_POOL));  /// [Note]: The payable version of this contract address
+    }
 
     /**
      * @notice - An organization deposit ETH into the Organization Pool
      */
     function depositETH() public payable returns (bool) {
-        address payable orgAddress = msg.sender;
-        orgAddress.transfer(msg.value);
+        uint amountDeposited = 1 ether;  /// [Note]: This is an example amount of deposit of ETH 
+
+        /// Receive specified-amount of Ether
+        ORG_POOL_PAYABLE.call.value(amountDeposited).gas(53000)("");   
+        //ORG_POOL_PAYABLE.call{value: amountDeposited}.gas(35000)();
+
+        //require(address(this).call.value(msg.value).gas(35000)(), "Transferring ETH was fail");
     }
 
     /**
@@ -22,7 +33,7 @@ contract OrgPool {
      */
     function depositETHFromAllOrgs() public payable returns (bool) {
         /// [Todo]: Add a logic
-        address[] payable orgAddresses;
+        address[] memory orgAddresses;
         uint amountOfDeposit;  /// Amount of deposit of each organization
     }
 
@@ -30,6 +41,7 @@ contract OrgPool {
      * @notice - Share gas cost between registered-organizations
      */
     function gasCostSharing() public returns (bool) {
+
 
     }
 
